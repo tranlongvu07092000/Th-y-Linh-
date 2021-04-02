8fd8d643037b8f60ae616db03409656bade4dd94
longvu07092000 <exhateme0000@gmail.com>
2021-04-02 21:13:11 +0700

    Bai_Tap_Ve_Nha

---
+++ bai_tap1/Bai_tap_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	float a,b;
+	
+	printf ("Nhap so tien bang do la:"); scanf("%f",&a);
+	b = a*16689;
+	printf("Bang tien viet la:%.3f",b);
+	
+	return 0;
+}
